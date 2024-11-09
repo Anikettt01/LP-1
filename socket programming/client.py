e755ea984dcb554bfd3349e063dcc2f4b54fa293
Aniket Deore <aniketrdeore@gmail.com>
2024-11-09 19:36:53 +0530

    added algorithm
    added algorithm using comments for easy understanding

--- socket programming/client.py
+++ socket programming/client.py
@@ -1,32 +1,40 @@
+# import socket and threading
+# add server ip and port
+
+# create a fucntion for receiving the message use while loop with true condition if message is there then simply print it else connection break
+
+#define main function create socket for client and connect it with ip and port number create thread for receiving message use a while loop with true
+# condition and take input for message now if message is quit break the loop else send the messages at the end close the server 
+
+
 import socket
 import threading
 
-SERVER_IP = '127.0.0.1'  # Change this if needed
+SERVER_IP  = '127.0.0.1'
 PORT = 8080
 
-def receive_messages(client_socket):
+def receive_message(client_socket):
     while True:
         try:
             message = client_socket.recv(1024)
             print(message.decode())
         except:
-            print("Connection to the server closed.")
+            print("Connection to the server is closed.")
             client_socket.close()
             break
 
 def main():
-    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    client.connect((SERVER_IP, PORT))
+    client = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+    client.connect((SERVER_IP,PORT))
 
-    receive_thread = threading.Thread(target=receive_messages, args=(client,))
+    receive_thread = threading.Thread(target=receive_message,args=(client,))
     receive_thread.start()
 
     while True:
-        message = input("Enter a message to broadcast: ")
-        if message.lower() == 'quit':
+        message = input("Enter message to broadcast: ")
+        if(message.lower()=='quit'):
             break
-        client.send(message.encode())  # Send message to the server
-
+        client.send(message.encode())
     client.close()
 
 if __name__ == '__main__':
--- socket programming/server.py
+++ socket programming/server.py
@@ -1,12 +1,28 @@
+# import socket and threading1
+
+# add host and port number
+
+# create a array clients which will store all the client
+
+# define broadcast function which will be simply decide to whome the message should display we will simply use one loop on array and if client now 
+# equal to sender_socket then send message else dont send as we dont sent the message to the client which is sening message
+
+# create one handler for client which will simple use loop which is always true and will take 1024 buffer size for the message if message is there
+# then print it and send to all the client by simply calling broadcast function
+
+# define main create a server socket  we are keeping limit of 5 for the number of clients start a true condition loop create a client thread and start it
+
+# finally define main function
+
 import socket
 import threading
 
-HOST = '127.0.0.1'  
+HOST = '127.0.0.1'
 PORT = 8080
 
 clients = []
 
-def broadcast(message, sender_socket):
+def broadcast(message,sender_socket):
     for client in clients:
         if client != sender_socket:
             try:
@@ -20,23 +36,23 @@ def client_handler(client_socket):
             message = client_socket.recv(1024)
             if not message:
                 break
-            print(f"Received: {message.decode()}")  # Print received message
-            broadcast(message, client_socket)
+            print(f"Received: {message.decode()}")
+            broadcast(message,client_socket)
         except:
             break
     client_socket.close()
 
 def main():
-    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server.bind((HOST, PORT))
+    server = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+    server.bind((HOST,PORT))
+
     server.listen(5)
-    print(f'Server listening on {HOST}:{PORT}')
+    print(f'server listening on {HOST}:{PORT}')
 
     while True:
-        client_socket, addr = server.accept()
-        print(f'Accepted connection from {addr[0]}:{addr[1]}')
+        client_socket,addr = server.accept()
         clients.append(client_socket)
-        client_thread = threading.Thread(target=client_handler, args=(client_socket,))
+        client_thread = threading.Thread(target=client_handler,args=(client_socket,))
         client_thread.start()
 
 if __name__ == '__main__':
